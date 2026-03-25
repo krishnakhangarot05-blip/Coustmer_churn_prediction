d0184c8ba5ab4fd9459d84f129377b9ec701ddbf
krishnakhangarot05-blip <krishnakhangarot05@gmail.com>
2026-03-25 14:12:10 +0530

    app.py

--- app.py
+++ app.py
@@ -1,35 +1,47 @@
 import streamlit as st
 import pickle
-import numpy as np
+import pandas as pd
+
 
 model = pickle.load(open("churn_model.pkl", "rb"))
+columns = pickle.load(open("columns.pkl", "rb"))
 
 st.title("Customer Churn Prediction")
 
 st.write("Enter Customer Details:")
 
-
+# Inputs
 gender = st.selectbox("Gender", ["Male", "Female"])
-tenure = st.number_input("Tenure (months)", min_value=0)
+tenure = st.number_input("Tenure", min_value=0)
 monthly_charges = st.number_input("Monthly Charges")
 total_charges = st.number_input("Total Charges")
 
-contract = st.selectbox("Contract Type", ["Month-to-month", "One year", "Two year"])
+contract = st.selectbox("Contract", ["Month-to-month", "One year", "Two year"])
 
-#encoding
-gender = 1 if gender == "Male" else 0
+# Convert input into dictionary
+input_data = {
+    "tenure": tenure,
+    "MonthlyCharges": monthly_charges,
+    "TotalCharges": total_charges,
+}
 
-contract_one = 1 if contract == "One year" else 0
-contract_two = 1 if contract == "Two year" else 0
+# Encoding (must match training)
+input_data["gender_Male"] = 1 if gender == "Male" else 0
+input_data["Contract_One year"] = 1 if contract == "One year" else 0
+input_data["Contract_Two year"] = 1 if contract == "Two year" else 0
+
+# Convert to DataFrame
+input_df = pd.DataFrame([input_data])
 
 
+input_df = input_df.reindex(columns=columns, fill_value=0)
+
+# Prediction
 if st.button("Predict"):
-    data = np.array([[gender, tenure, monthly_charges, total_charges,
-                      contract_one, contract_two]])
-
-    pred = model.predict(data)
+    pred = model.predict(input_df)
 
     if pred[0] == 1:
         st.error(" Customer will Churn")
     else:
         st.success(" Customer will Stay")
+
